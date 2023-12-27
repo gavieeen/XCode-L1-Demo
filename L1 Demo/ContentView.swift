78826629219a01f7ecb4e6984c8d951b98adcdb1
gavieeen <gavinebenezer@gmail.com>
2023-12-27 03:36:23 -0600

    Getting more comfortable with Stacks and UI look

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -11,21 +11,65 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Color(.black)
+            Color(.red)
                 .ignoresSafeArea()
             
             VStack {
                 Image("obito")
                     .resizable()
-                    .cornerRadius(10)
+                    .cornerRadius(45)
                     .aspectRatio(contentMode: .fit)
-                    .padding(.all)
-                Text("gavieeen")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
+                    .overlay(
+                        Color.red
+                            .opacity(0.4)
+                            .cornerRadius(45)
+                            .blur(radius: 30)
+                            .blendMode(.multiply)
+                    )
+                
+                HStack {
+                    Image(systemName: "bolt.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(.purple)
+                        .shadow(color: .red, radius: 10, x: 3, y: 3)
+                    
+                    Text("gavieeen")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.red)
+                        .shadow(color: .purple, radius: 3, x: 3, y: 3)
+                        .overlay(
+                            Color.black
+                                .opacity(0.3)
+                                .blur(radius: 5)
+                                .blendMode(.multiply)
+                        )
+                    
+                    Image(systemName: "bolt.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(.purple)
+                        .shadow(color: .red, radius: 10, x: 3, y: 3)
+
+                }
+                
+                HStack {
+                    Image(systemName: "centsign.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(.purple)
+                        .shadow(color: .red, radius: 10, x: 3, y: 3)
+                    Image(systemName: "dollarsign.square")
+                        .font(.largeTitle)
+                        .foregroundColor(.purple)
+                        .shadow(color: .red, radius: 10, x: 3, y: 3)
+                }
             }
-            
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.black)
+                .cornerRadius(45)
+                .shadow(color: .black, radius: 45)
+            )
+            .padding()
         }
         
     }
